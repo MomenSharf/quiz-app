6dd4e2f40725c60789de4c0ac151a6170f6c36fb
Momen Sharf <mwmnshrfaldinps3git config --global user.email mwmnshrfaldinps3@gmail.comgit config --global user.email 'mwmnshrfaldinps3git config --global user.name 'Momen Sharf>
2024-11-26 19:27:46 +0200

    ...

--- app/(root)/dashboard2/[quizId]/page.tsx
+++ app/(root)/dashboard2/[quizId]/page.tsx
@@ -1,17 +1,17 @@
-import { getCurrentUser } from '@/lib/auth';
-import { notFound, redirect } from 'next/navigation';
-import React from 'react'
+// import { getCurrentUser } from '@/lib/auth';
+// import { notFound, redirect } from 'next/navigation';
+// import React from 'react'
 
-export default async function page({
-  params: { quizId },
-}: {
-  params: { quizId: string };
-}) {
+// export default async function page({
+//   params: { quizId },
+// }: {
+//   params: { quizId: string };
+// }) {
 
   
-  return (
-    <div>
+//   return (
+//     <div>
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
