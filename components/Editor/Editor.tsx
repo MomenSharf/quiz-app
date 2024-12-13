89c4448a8da2e98dfb6a0d805d8400d685aa41f6
MomenSharf <mwmnshrfaldinps3@gmail.com>
2024-12-13 18:20:30 +0200

    ...

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header/Header";
-import Sidebar from "./Sidebar/Sidebar";
 import Content from "./Content";
+import Sidebar from "./Sidebar/Sidebar";
 
 export default function Editor() {
   return (
