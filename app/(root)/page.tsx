e6537508ef8f6d4b2ac68c05fd57bd3fb2a94ec4
MomenSharf <mwmnshrfaldinps3@gmail.com>
2025-02-12 19:05:12 +0200

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,16 +7,16 @@ import { getSearchQuizzes } from "@/lib/actions/search";
 import { db } from "@/lib/db";
 
 export default async function Home() {
-  const result = await getHomeQuizzes();
+  // const result = await getHomeQuizzes();
 
-  const gg = result?.filter((e) => e !== null);
+  // const gg = result?.filter((e) => e !== null);
 
   return (
     <div className="flex flex-col">
       <div className="container p-2 sm:p-4 mt-1 sm:mt-3 mb-5 flex flex-col gap-5">
         <Cards />
         <CategoriesScroller />
-        {gg &&
+        {/* {gg &&
           gg.map(({ title, quizzes,route }) => {
             return (
               <QuizzesCardsScroller
@@ -26,7 +26,7 @@ export default async function Home() {
                 route={route}
               />
             );
-          })}
+          })} */}
       </div>
     </div>
   );
