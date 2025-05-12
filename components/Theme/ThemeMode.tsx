b42322957865efd2f79e90ec40b3b64466c6c923
MomenSharf <mwmnshrfaldinps3@gmail.com>
2025-05-12 09:33:00 +0300

    update

--- components/Theme/ThemeMode.tsx
+++ components/Theme/ThemeMode.tsx
@@ -8,7 +8,7 @@ export default function ThemeMode() {
 
   return (
     <div className=" relative flex justify-center items-center min-w-6">
-      <button onClick={() => dispatch({ type: "TOGGLED_MODE", payload: null })}>
+      <button onClick={() => dispatch({ type: "TOGGLED_MODE", })}>
         <SunIcon
           className={cn(
             "text-foreground hover:text-primary transition-all delay-100 absolute left-0 top-0 translate-x-1/2 -translate-y-1/2  sm:-translate-y-1/2",
